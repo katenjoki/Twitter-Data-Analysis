0aee3cf131469d69b398b1cdaa51104127a20122
katenjoki <62309540+katenjoki@users.noreply.github.com>
2021-06-28 22:44:02 +0300

    Update extract_dataframe.py

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -57,8 +57,8 @@ class TweetDfExtractor:
         for t in text:
             if (t):
                 sentiments=TextBlob().sentiment
-                polarity.append(t[sentiments.polarity])
-                subectivity.append(t[sentiments.subjectivity])              
+                polarity.append(sentiments.polarity)
+                subectivity.append(sentiments.subjectivity)              
         return polarity, subjectivity
 
     def find_created_time(self)->list:
